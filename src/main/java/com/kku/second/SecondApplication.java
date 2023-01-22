b73ebab16d9da57173e54647fe01c9c0385f6c81
kondisettysaidheeraj <105906908+kondisettysaidheeraj@users.noreply.github.com>
2023-01-22 13:11:37 +0530

    second commit

--- src/main/java/com/kku/second/SecondApplication.java
+++ src/main/java/com/kku/second/SecondApplication.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.kku.second.dao.Counter;
+
 
 @SpringBootApplication
 public class SecondApplication {
